59274f776dd41872b5f30957d84fd08325635441
boscahya <boscahya@gmail.com>
2020-08-30 17:37:43 +0700

    ww
    ww

--- Permainan/towerblocks/dist/profil.js
+++ Permainan/towerblocks/dist/profil.js
@@ -5,9 +5,10 @@
 	document.head.insertAdjacentHTML('beforeend','<style>.sg { width: 35px; height: 35px; position: fixed; bottom: 10px; right: 10px; } .sg .eye { -webkit-transform: translateX(0px);   transform: translateX(0px); } .sg:hover .eye { -webkit-transition: -webkit-transform 0.3s ease; transition: -webkit-transform 0.3s ease; transition: transform 0.3s ease; transition: transform 0.3s ease, -webkit-transform 0.3s ease; -webkit-transform: translateX(12px);   transform: translateX(12px); }</style>'); 
   
 	var a = document.createElement('a');
-	a.setAttribute('href','https://twitter.com/pranandaarif');
+	a.setAttribute('href','https://boscahya.github.io/');
 	a.setAttribute('target','_blank');
 	a.innerHTML = icon;
     
  	document.body.appendChild(a);
 })();
+
